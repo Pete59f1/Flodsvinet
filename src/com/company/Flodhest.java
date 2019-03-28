0ba04bd73850192c4dbfb568cdd512f24836d32d
Peter Starup <pete59f1@edu.eal.dk>
2019-03-28 14:01:02 +0100

    Flodhest

--- src/com/company/Flodhest.java
+++ src/com/company/Flodhest.java
@@ -5,6 +5,24 @@ public class Flodhest {
     private Integer antalTænder;
     private Integer vægt;
 
+    public Flodhest(String lugt, Integer antalTænder, Integer vægt){
+        this.lugt = lugt;
+        this.antalTænder = antalTænder;
+        this.vægt = vægt;
+    }
 
+    public int FåVægt(String lugt){
+        Integer vægt = 0;
+        if (lugt.equals("Lort")){
+            vægt = 600;
+        }
+        else if (lugt.equals("Pis")){
+            vægt = 300;
+        }
+        else{
+            vægt = 1000;
+        }
+        return vægt;
+    }
 
 }
